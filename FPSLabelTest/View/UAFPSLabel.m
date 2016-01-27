0198075ef6cd08a3dc40d6bd017bb1053919161d
上海－郭朝阳 <guochaoyang@jd.com>
2016-01-28 00:08:40 +0800

    fps

---
+++ FPSLabelTest/View/UAFPSLabel.m
@@ -0,0 +1,23 @@
+//
+//  UAFPSLabel.m
+//  FPSLabelTest
+//
+//  Created by ccguo on 16/1/27.
+//  Copyright © 2016年 yangyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FPSLabel:UILabel
+
+@property (nonatomic,strong) CADisplayLink *displayLink;
+
+
+@end
+
+__attribute__((constructor)) static void DWURecyclingAlert(void) {
+    @autoreleasepool {
+        NSLog(@"startFps");
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# FPSLabel
+
+移植以下两个大神的代码：
+DXFPSLabel：xiekw2010 链接：https://github.com/xiekw2010/DXFPSLabel
+RRFPSBar:RolandasRazma 链接：https://github.com/RolandasRazma/RRFPSBar
+
+An easy label use to show screen fps. 
+You can use "FPSLabel showInWindow:(UIWindow *)window" to display fps on screen with your app.
